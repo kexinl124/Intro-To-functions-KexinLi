5df197568f5f99f053aeb6d7074162eb8a2a0a69
kexinl124 <KExinl10@nycstudents.net>
2025-10-06 17:59:54 -0400

    /

--- test.py
+++ test.py
@@ -48,12 +48,3 @@ def equal(x):
    t.forward(x)
    t.left(120)
 equal(90)
-
-#Assesment 1
-sidelength = 100
-rotate = 90
-def square(x,y):
-    for i in range(4):
-        t.forward(x)
-        t.left(y)
-square(100,90)
